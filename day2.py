d4ff9f940f07d7b1eaecaf1ed018f02c418ea389
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-02-01 14:57:10 +0530

    Escape sequence and s/m line comments

---
+++ day2.py
@@ -0,0 +1,8 @@
+'''*/heuyy please dont 
+,nj,vnm''' #multiline comments
+print("hey I am good girl \n and this vier is good boy and girls")
+print("") #unexcuted code
+print("hey I \'Aayushi\' \n and I am good girl")
+#\" \" for double quotes and single quotes
+print("hey ",6,8,sep="+",end="\n Aayushi")
+print("hekko")
