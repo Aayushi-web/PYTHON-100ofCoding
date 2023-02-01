ada134932b1a0803c265e54cdab63bbaa7c76179
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-02-01 15:57:58 +0530

    list,dic,tuple

--- day3.py
+++ day3.py
@@ -1,6 +1,17 @@
 a="Aayushi"
-b=5
+b=5.1
 c=None
 d=True
+e= complex(1,5)
 print(a,b,c,d)
 print(type(b))
+print(e)
+list1=[8,5,2,["aayushi"],["hfjdj","jyantii"],None]
+print(list1)
+print(type(list1))
+
+t="aayushi","jhsdkj","kdjkld"
+print(type(t))
+map={"name":"aauisho","age":20,"canVote":True}
+print(map)
+print(type(map))
