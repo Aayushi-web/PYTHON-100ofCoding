13a227ea63ce0cf48f56d1b99cea184e779fed90
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-02-03 14:00:46 +0530

    create: String.py

---
+++ Strings.py
@@ -0,0 +1,23 @@
+name="Aayushi"
+name1="Mishra"
+print("hey My Name is ",name + name1)
+
+#'''.....''' are for multi line strings
+sentence='''hello my name is Aayushi Mishra and I am a good girl and I am learning python for my  Academics...'''
+print(sentence)
+print(name[5])
+# indexing start with 0
+# for character in sentence:
+#     print(character)
+    #string slicing
+sen="Sweta"
+print(sentence[1:9])
+print(sentence[:9])
+print("sentence is a ",len(sentence))
+#string is a type of array we can access any index of an array using[]
+print(sentence[:])
+# Negative slicing
+print(sentence[0:-4])
+print(name[-3:-1])
+nm="Harry"
+print(nm[-4:-2])
