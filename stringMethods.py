4898f5b5724d070544b88072a7f67bf24286f5f9
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-02-05 01:57:58 +0530

    create:Stringmethods.py

---
+++ stringMethods.py
@@ -0,0 +1,8 @@
+a="Aayushizzzzzz"
+print(a)
+print(len(a))
+print(a.rstrip("z"))
+print(a.upper())
+print(a.lower())
+print(a.format())
+#strings are immutable in nature we canot change the value but we can modify it every time changed strimng will re created in memory
