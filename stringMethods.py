d1107e7dd8e81de4e883b77eb07b22358d736b6a
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-02-05 02:31:46 +0530

    changesss

--- stringMethods.py
+++ stringMethods.py
@@ -1,8 +1,14 @@
-a="Aayushizzzzzz"
+a="Aayu  shiz  zzzzz"
 print(a)
 print(len(a))
 print(a.rstrip("z"))
 print(a.upper())
 print(a.lower())
 print(a.format())
+print(a.replace("Aayushi","Anishka"))
 #strings are immutable in nature we canot change the value but we can modify it every time changed strimng will re created in memory
+print(a.split(" "))
+heading="introduction to heading my a"
+print(heading.capitalize())
+print(a.casefold())
+print(a.encode())
