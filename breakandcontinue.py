615648046fcc76d13eca77dddb4692a8a3d24452
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-02-14 00:53:04 +0530

    create: breakeandcontinue.py

---
+++ breakandcontinue.py
@@ -0,0 +1,11 @@
+# for i in range(1,12):
+#     print("5 x",i, "=",5*i)
+#     if(i==10):
+#      continue
+# print("lopp ko chodke nikal gya")
+i=1
+while True:
+    print(i)
+    i=i+1
+    if(i%100==0):
+        break
