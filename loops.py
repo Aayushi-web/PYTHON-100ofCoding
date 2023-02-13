7555040f5b64efd61580faec96bb38546d4d51de
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-02-14 00:29:18 +0530

    update loops.py

--- loops.py
+++ loops.py
@@ -3,6 +3,17 @@
 # for x in color:
 #  print(x)
 
-for i in range(5,20,4): 
-  print(i)
+# for i in range(5,20,4): 
+#   print(i)
+# j= int(input("enter the number"))
+# while(j<=30):
+#     j= int(input("enter the number"))
+#     print(j)
+for i in range(4):
+    print(i)
+else:
+    print("I am outside else")
+  
+ 
+
     
