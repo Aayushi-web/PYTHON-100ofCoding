be5d56ffb15e94b2d6a59ba8109b655e1a009ec2
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-02-18 01:48:43 +0530

    create: argument.py

---
+++ argument.py
@@ -0,0 +1,15 @@
+
+def average(a,b):
+    return ((a+b)*2)
+a1=input("enter 1st")
+a2= input("enter 2nd")
+print(average(a1,a2))
+
+# def avg(*number):
+#     sum=0
+#     for i in number:
+#         sum+=i
+#         return(sum/len(number))
+
+# c= input()
+# print(avg(5,4,c))
