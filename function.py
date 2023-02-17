7d3140eed9f707d60d894244f3aaaada03c22ea2
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-02-18 01:15:11 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,19 @@
+def cal(a,b):
+    mean=(a*b)/(a+b)
+    print(mean)
+
+a=9
+b=8
+f=5
+def isgreater(a,b):
+    if(a>b):
+     print("a is greater than b")
+    else:
+     print("a is not greater than b")
+
+ 
+   
+cal(a,b)
+isgreater(f,b)
+def islesser():
+      pass
