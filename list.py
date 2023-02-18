df6c23580a8b3f713722c6125f486cbe50e8dadf
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-02-18 19:34:07 +0530

    update: list.py

--- list.py
+++ list.py
@@ -1,14 +1,34 @@
-l= [5,8,0,7,5,8,7,8,7,5,5]
+# l= [5,8,0,7,5,8,7,8,7,5,5]
+# print(l)
+# print(l[0])
+# name=["aayushi","anishka","aryan"]
+# print(name)
+# anything=["aayushi",[4,5,6]]
+# print(anything[1:])
+# if 7 in l:
+#     print("yess")
+# if "a" in "Anishka":
+#     print("yess")
+# else:
+#     print("nooo")        
+# print(l[1:8:3])
+# list =[i*i*i for i in range(4)]
+# print(list)
+# list method
+l=[1,2,3,4,5,6]
 print(l)
-print(l[0])
-name=["aayushi","anishka","aryan"]
-print(name)
-anything=["aayushi",[4,5,6]]
-print(anything[1:])
-if 7 in l:
-    print("yess")
-if "a" in "Anishka":
-    print("yess")
-else:
-    print("nooo")        
-print(l[1:8:3])
+l.append(5)
+print(l)
+l.sort(reverse=True)
+print(l)
+l.reverse()
+print(l)
+print(l.index(2))
+print(l.count(5))
+m= l.copy()
+print(m)
+l.insert(1,989)
+print(l)
+m=[455,78,56]
+l.extend(m)
+print(l)
