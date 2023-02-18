8ae0305691a5a55d8595203908d8fb1a8c834750
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-02-18 15:31:41 +0530

    create: list.py

---
+++ list.py
@@ -0,0 +1,13 @@
+l= [5,8,0,7]
+print(l)
+print(l[0])
+name=["aayushi","anishka","aryan"]
+print(name)
+anything=["aayushi",[4,5,6]]
+print(anything[1][1])
+if 7 in l:
+    print("yess")
+if "a" in "Anishka":
+    print("yess")
+else:
+    print("nooo")        
