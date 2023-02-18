e75ae3a6ca9810333115c4e8107a74691f29106d
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-02-19 01:57:52 +0530

    fstring.py

---
+++ fstring.py
@@ -0,0 +1,11 @@
+# first_name="Aayushi"
+# last_name="Mishra"
+# print(f"heyy My name is      "{first_name}"{last_name} ... Nice to meet you!!!")
+# txt=45.888999
+# print(f"hwlo{txt:.2f}")
+import this
+def square(n):
+    '''takes number n  and return square'''
+    print(n**2)
+square(5)
+print(square.__doc__)    
