f0890a2de41e0d709605916120369b87670a62d1
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-02-18 15:38:47 +0530

    done

--- list.py
+++ list.py
@@ -1,13 +1,14 @@
-l= [5,8,0,7]
+l= [5,8,0,7,5,8,7,8,7,5,5]
 print(l)
 print(l[0])
 name=["aayushi","anishka","aryan"]
 print(name)
 anything=["aayushi",[4,5,6]]
-print(anything[1][1])
+print(anything[1:])
 if 7 in l:
     print("yess")
 if "a" in "Anishka":
     print("yess")
 else:
     print("nooo")        
+print(l[1:8:3])
