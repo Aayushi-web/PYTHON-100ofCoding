f7a2dba9fc4d4b1a304f114e85d4a042976a62df
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-02-22 22:56:27 +0530

    update

--- final.py
+++ final.py
@@ -1,15 +1,24 @@
-def func1():
-    try:
-       l=[1,5,64,1]
-       i= int(input("enter your num"))
-       print(l[i])
-    except:
-       print("some error occur")
-       return 0
+# def func1():
+#     try:
+#        l=[1,5,64,1]
+#        i= int(input("enter your num"))
+#        print(l[i])
+#     except:
+#        print("some error occur")
+#        return 0
 
 
-    finally:
-     print("I am always executed ") 
+#     finally:
+#      print("I am always executed ") 
 
-x= func1()
-print(x)
+# x= func1()
+# print(x)
+# custom errors
+
+a = (input("enter quit word"))
+
+if(a):
+    print("exit")
+
+else:    
+    raise ValueError("values should be between 5 and 9")
