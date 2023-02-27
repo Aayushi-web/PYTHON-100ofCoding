9840097a8c1d6252c32be1cec8ef2a43f9358099
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-02-28 02:55:29 +0530

    update: shortend.py

--- shortend.py
+++ shortend.py
@@ -1,4 +1,7 @@
-a=20
-b=10
-print("A is greayter") if(a>b) else "A is not greater than b"
-print(f"the value of A is {a} ")
+# a=20
+# b=10
+# print("A is greayter") if(a>b) else "A is not greater than b"
+# print(f"the value of A is {a} ")
+
+from  math import *
+print(sqrt(36)*pi)
