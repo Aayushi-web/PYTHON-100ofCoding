6c10a3d0ba1f13114dd13bf0ecb38e2c6f62edef
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-03-09 03:51:23 +0530

    create:map.py

---
+++ map.py
@@ -0,0 +1,19 @@
+# map
+
+# def avg(a):
+#     return (a)/2
+# li=[2,10,18,20]
+# # print (int(list(map(avg,li))))
+
+# # filter
+# def fil_func(a):
+#     return a>4
+# newline= filter(fil_func,li)
+# print (list((newline)))
+
+
+# # reduce
+from functools import reduce
+num=[1,2,3,4,5]
+sum= reduce(lambda x,y: x+y, num)
+print(sum)
