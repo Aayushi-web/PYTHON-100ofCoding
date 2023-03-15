7b7374f89d8fa704ec632e9c8dcf8eb93fb6e015
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-03-15 13:59:11 +0530

    xhanges

---
+++ accesmodifier.py
@@ -0,0 +1,11 @@
+class Employee:
+    def __init__(self):
+        self.name="Aayushi"
+a=Employee()
+# print(a.__name) cnot acess dirrectly 
+
+# print(a._Employee__name) #this can acdess indirectly
+# print(a.__dir__())
+class Tech(Employee)
+
+a.Tech()
---
+++ snake,wqter,gun.py
@@ -0,0 +1,16 @@
+import random
+op="swg"
+
+
+
+
+
+
+
+computer=random.randint(0,2)
+user=str((input(" 0 for water , 1 for snake, 2 for gun ")))
+
+
+
+
+
